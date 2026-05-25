9230c7f94653a0721188e4e5af1e4cede3bb09b3
mk <mingkang@user.noreply.gitee.com>
2026-05-25 16:53:49 +0800

    add config.example.js

---
+++ config.example.js
@@ -0,0 +1,6 @@
+// Copy this file to config.js and fill in your Telegram credentials.
+// config.js is gitignored and will never be committed.
+var TG = {
+  token: 'YOUR_BOT_TOKEN',
+  chatId: 'YOUR_CHAT_ID'
+};
